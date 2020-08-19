c4822a1abc1f5d27447b20df93b8a7ccb4591c2c
Minimal Structure <69333458+MinimalStructure@users.noreply.github.com>
2020-08-19 09:24:52 -0700

    Create sat.bat

---
+++ sat.bat
@@ -0,0 +1,10 @@
+@py c:\users\marc\mypythonscripts\sats_counter2.py %*
+@pause
+
+'''
+
+this bat file allows me to run the program by using windows r and typing sat in it.
+it requuires an intial setup.  I leared about it here:  https://automatetheboringstuff.com/2e/chapter9/
+
+
+'''
